4af4d9666c6a6559021e160528836ef36c724471
Sergei Korshunov <korshunov.sergei.anatolevich@mail.ru>
2025-07-21 10:43:04 +0300

    Поправил codestyle в классе UserService.

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
